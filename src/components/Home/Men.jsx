00e274df4393fe3b86aa852861f75bf90a7a8f50
momin096 <momin09609@gmail.com>
2025-08-01 14:52:17 +0600

    men collection & recommended section added

--- src/components/Home/Men.jsx
+++ src/components/Home/Men.jsx
@@ -1,10 +1,10 @@
-import  { useEffect, useState } from 'react';
-import ProductCard from '../ProductCard/ProductCard';
+import { useEffect, useState } from "react";
+import ProductCard from "../ProductCard/ProductCard";
 
-const Women = () => {
+const Men = () => {
     const [products, setProducts] = useState([])
     useEffect(() => {
-        fetch('/women.json')
+        fetch('/men.json')
             .then((res) => res.json())
             .then((data) => setProducts(data))
     }, [])
@@ -12,7 +12,7 @@ const Women = () => {
         // protom line e items-center hobe mobile device e center e anar jonne 
         <div className="container mx-auto my-10 flex flex-col py-10 ">
             <div className="flex flex-col items-center md:flex-row md:justify-between md:items-center mb-5">
-                <h2 className="text-xl lg:text-3xl mb-5 font-medium">Women Collection</h2>
+                <h2 className="text-xl lg:text-3xl mb-5 font-medium">Men Collection</h2>
                 <button className="bg-amber-500 px-5 py-2 rounded-md font-bold text-white">View All</button>
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-5 items-center  ">
@@ -25,4 +25,4 @@ const Women = () => {
     );
 };
 
-export default Women;
+export default Men;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import Banner from "../components/Home/Banner";
 import Category from "../components/Home/Category";
 import DiscountProducts from "../components/Home/DiscountProducts";
+import Men from "../components/Home/Men";
+import Recommended from "../components/Home/Recommended";
 import Women from "../components/Home/Women";
 import ProductCard from "../components/ProductCard/ProductCard";
 
@@ -12,6 +14,9 @@ const Home = () => {
             <div className="bg-gray-100">
                 <DiscountProducts />
                 <Women />
+                <Men />
+
+                <Recommended />
             </div>
         </div>
     );
